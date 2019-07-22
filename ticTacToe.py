4687a7dccd393f4a3dc24e68d2771fbbcbe68eca
sjr19 <47469319+sjr19@users.noreply.github.com>
2019-07-22 15:25:27 -0700

    changes made here, the game is complete!

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,69 +1,106 @@
- print()
- print('-----------------------')
- print('Welcome to Tic Tac Toe!')
- print('-----------------------')
- print()
- player1 = ''
- player2 = ''
- print('Player 1: Do you want to be X or O?')
- # Input the value and set values of player1 and player2
- print('Player 1 will go first.')
+# Functions
+#################################################################
 
- inputVal = ''
- print('Are you ready to play? Enter Yes or No.')
- # Input whether they are ready to player1
-
-board = {1 : ' ', 2 : ' ', 3 : ' ', 4 : ' ', 5 : ' ', 6 : ' ', 7 : ' ', 8 : ' ', 9 : ' '}
-
- while(inputVal == 'Yes'):
-   # Player 1
-   printBoard()
-   player1Input = 0;
-   print('Choose your next position: (1-9)')
-   # Input the next position to enter
-
-   flag = True;
-   while(flag):
-     if (player1Input > 0 and player1Input < 10):
-       if board.get(player1Input) != '':
-         print('The position already exists')
-       else:
-         board.set(player1Input, player1)
-         flag = False
-     else:
-       print('The inputed value does not exist between 1 - 9')
-  
-   evaluateBoard()
-
-   # Player 2
-   printBoard()
-   player2Input = 0;
-   print('Choose your next position: (1-9)')
-   # Input the next position to enter
-
-   flag = True;
-   while(flag):
-     if (player2Input > 0 and player2Input < 10):
-       if board.get(player2Input) != '':
-         print('The position already exists')
-       else:
-         board.set(player2Input, player2)
-         flag = False
-     else:
-       print('The inputed value does not exist between 1 - 9')
-  
-   evaluateBoard()
-
-
+# Prints the current tic tac toe board
 def printBoard():
-    print('-------------------')
-    print('| ', board.get(7), ' | ',  board.get(8), ' | ', board.get(9), ' |    ', ' 7 | 8 | 9 ')
-    print('-------------------      ----------')
-    print('| ', board.get(4), ' | ',  board.get(5), ' | ', board.get(6), ' |    ', ' 4 | 5 | 6 ')
-    print('-------------------      ----------')
-    print('| ', board.get(1), ' | ',  board.get(2), ' | ', board.get(3), ' |    ', ' 1 | 2 | 3 ')
-    print('-------------------')
+  print()
+  print(' ' + board[0] + ' ' + '|' + ' ' + board[1] + ' ' + '|' + ' ' + board[2] + ' ')
+  print('-----------')
+  print(' ' + board[3] + ' ' + '|' + ' ' + board[4] + ' ' + '|' + ' ' + board[5] + ' ')
+  print('-----------')
+  print(' ' + board[6] + ' ' + '|' + ' ' + board[7] + ' ' + '|' + ' ' + board[8] + ' ')
+  print()
+  
+# Takes the input of both users 
+def inputPosition(player):
+  position = int(input(player + ': Choose your next position: (1 - 9): '))
+  if(player == 'Player 1' and board[position - 1] == ' '):
+    board[position - 1] = p1
+    return False
+  elif(player == 'Player 2' and board[position - 1] == ' '):
+    board[position - 1] = p2
+    return False
+  else:
+    return True
+  
+# Checks if a user has won the particular round
+def checkWinner():
+  if (board[0] == board[1] and board[0] == board[2]):
+    winner = board[0]
+  elif (board[3] == board[4] and board[3] == board[5]):
+    winner = board[3]
+  elif (board[6] == board[7] and board[6] == board[8]):
+    winner = board[6]
+  elif (board[0] == board[3] and board[0] == board[6]):
+    winner = board[0]
+  elif (board[1] == board[4] and board[1] == board[7]):
+    winner = board[1]
+  elif (board[2] == board[5] and board[2] == board[8]):
+    winner = board[2]
+  elif (board[0] == board[4] and board[0] == board[8]):
+    winner = board[0]
+  elif (board[2] == board[4] and board[2] == board[6]):
+    winner = board[2]
+  else:
+    winner = ' '
+    
+  if (p1 == winner):
+    print('The winner is Player 1')
+    print()
+    return True
+  elif (p2 == winner):
+    print('The winner is Player 2')
+    print()
+    return True
+  else:
+    return False
+    
+# Main
+#################################################################
 
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+p1 = ' '
+p2 = ' '
+p1_input = ' '
+p2_input = ' '
 
-def evaluateBoard():
-  # Need to figure out the evaluating part
+print('Welcome to Tic Tac Toe!')
+print()
+
+p1 = input('Player 1: Do you want to be X or O? ')
+
+if (p1 == 'X'):
+  p2 = 'O'
+else:
+  p2 = 'X'
+
+print()
+validation = input('Are you ready to play? Enter Yes or No. ')
+print()
+print('=======================================')
+print()
+
+while (validation == 'Yes'):
+  validInput = True 
+  while (validInput):
+    validInput = inputPosition('Player 1')
+  
+  printBoard()
+  check = checkWinner()
+  
+  if(check == True):
+    break
+    
+  validInput = True
+  while (validInput):
+    validInput = inputPosition('Player 2')
+  printBoard()
+  check = checkWinner()
+  
+  if(check == True):
+    break
+    
+print()
+print('=======================================')
+print()
+  
